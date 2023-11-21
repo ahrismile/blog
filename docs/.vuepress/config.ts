2a4bafc32234563b40207210fbd34aa58bec3f60
ahri <2717791678@qq.com>
2023-11-21 13:48:59 +0800

    why also trace dict/

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,7 +1,19 @@
-import { defineUserConfig } from 'vuepress'
+import { defaultTheme, defineUserConfig } from 'vuepress'
 
 export default defineUserConfig({
   lang: 'zh-CN',
   title: 'ahri\'s blog',
   description: '这是我的第一个 VuePress 站点',
+  theme: defaultTheme({
+    navbar:[
+      {
+        text:'home',
+        link:'/',
+      },
+      {
+        text:'record',
+        link:"/record/",
+      }
+    ]
+  })
 })
